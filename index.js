602f214478114ede0c678fa95eec908910da7432
lxxxvi <lxxxvi@users.noreply.github.com>
2018-07-28 12:07:47 +0200

    init

---
+++ index.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded",function(){
+  const click = ((document.ontouchstart !== null) ? 'click' : 'touchstart');
+  const bpmButton = document.querySelector("button");
+  const timestamp = document.querySelector("input");
+  const bpmOutput = document.querySelector("output");
+  const MILISECONDS_IN_A_MINUTE = 60000;
+
+  const refresh = function() {
+    let nextTimestamp = Date.now();
+    let previousTimestamp = getPreviousTimestamp();
+    let bpm = calculateBpm(previousTimestamp, nextTimestamp);
+
+    setBpm(bpm);
+    setTimestamp(nextTimestamp);
+  }
+
+  const calculateBpm = function(previousTimestamp, nextTimestamp) {
+    let previous = parseInt(previousTimestamp) || 0;
+    let next     = parseInt(nextTimestamp) || 0;
+
+    if (previous > 0 && next > 0) {
+      let milisecondsDifference = (next - previous);
+
+      return Math.round(MILISECONDS_IN_A_MINUTE / milisecondsDifference);
+    }
+
+    return '...';
+  }
+
+  const setBpm = function(value) {
+    bpmOutput.value = value;
+  }
+
+  const getPreviousTimestamp = function() {
+    return timestamp.value;
+  }
+
+  const setTimestamp = function(value) {
+    timestamp.value = value;
+  }
+
+  bpmButton.addEventListener(click, refresh, { passive: false });
+});
---
+++ sw.js
@@ -0,0 +1,29 @@
+var cacheName = 'bpm-v1';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/index.js',
+  '/styles.css'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate',  event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, {ignoreSearch:true}).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
