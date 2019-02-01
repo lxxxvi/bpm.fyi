320d1c1aec250ed837932d2fe1846941e234f71b
lxxxvi <lxxxvi@users.noreply.github.com>
2019-02-01 17:20:14 +0100

    improve service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var cacheName = 'bpm-v5';
+var cacheName = 'bpm-v6';
 var filesToCache = [
   '/',
   '/index.html',
@@ -16,11 +16,24 @@ self.addEventListener('install', function(e) {
   );
 });
 
-self.addEventListener('activate',  event => {
-  event.waitUntil(self.clients.claim());
+self.addEventListener('activate', event => {
+  console.log('[ServiceWorker] Activate');
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+
+  return self.clients.claim();
 });
 
 self.addEventListener('fetch', event => {
+  console.log('[ServiceWorker] Fetch', event.request.url);
   event.respondWith(
     caches.match(event.request, {ignoreSearch:true}).then(response => {
       return response || fetch(event.request);
